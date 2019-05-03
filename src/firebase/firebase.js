ee73eaee1ac21ff026c33225bed50fb688c55731
Michel Ferry <michelferry19@gmail.com>
2019-05-03 18:25:44 +0200

    added env variables

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,12 +1,12 @@
 import * as firebase from 'firebase';
 
 const config = {
-  apiKey: "AIzaSyDLAiLY6M0A7BvoeyC0h-WeCTV0adjPKb0",
-  authDomain: "expensify-d0a0d.firebaseapp.com",
-  databaseURL: "https://expensify-d0a0d.firebaseio.com",
-  projectId: "expensify-d0a0d",
-  storageBucket: "expensify-d0a0d.appspot.com",
-  messagingSenderId: "1002542239049"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
 };
 
 firebase.initializeApp(config);
