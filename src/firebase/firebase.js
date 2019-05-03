cacbc03a373f870526c24a6c595aa2270ae325d9
Michel Ferry <michelferry19@gmail.com>
2019-05-03 11:11:38 +0200

    Link to Firebase

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,16 +1,16 @@
 import * as firebase from 'firebase';
 
 const config = {
-  apiKey: "AIzaSyAapA5P8gWC83aHK26xgDDMzIBC4dw914w",
-  authDomain: "expensify-a13e6.firebaseapp.com",
-  databaseURL: "https://expensify-a13e6.firebaseio.com",
-  projectId: "expensify-a13e6",
-  storageBucket: "expensify-a13e6.appspot.com",
-  messagingSenderId: "124728096317"
+  apiKey: "AIzaSyDLAiLY6M0A7BvoeyC0h-WeCTV0adjPKb0",
+  authDomain: "expensify-d0a0d.firebaseapp.com",
+  databaseURL: "https://expensify-d0a0d.firebaseio.com",
+  projectId: "expensify-d0a0d",
+  storageBucket: "expensify-d0a0d.appspot.com",
+  messagingSenderId: "1002542239049"
 };
 
 firebase.initializeApp(config);
 
-firebase.database().ref().set({
-  name: 'Andrew Mead'
-});
+const database = firebase.database();
+
+export {firebase, database as default};
